ad552e472b7fc55fc23d08b78ca9f7c952c340b2
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2025-04-14 19:19:05 +0200

    feat: adds VacationRequest Model Class and makes CRUD pages also

---
+++ GotorzProjectMain/GotorzProjectMain/Models/VacationRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GotorzProjectMain.Models
+{
+	public class VacationRequest
+	{
+		[Key]
+		public int VacationRequestId { get; set; }
+		public string Country { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public int ChildrenAmount { get; set; }
+		public int AdultsAmount { get; set; }
+		public int RoomsAmount { get; set; }
+		public string HotelRequest { get; set; }
+		public string FlightRequest { get; set; }
+		public string Misc { get; set; }
+		public Status Status { get; set; }
+
+		public string UserId { get; set; }
+		public Customer Customer { get; set; }
+	}
+
+	public enum Status
+	{
+		PendingRequest,
+		WatingApproval,
+		BookingUnderway,
+		Done
+	}
+}
--- GotorzProjectMain/GotorzProjectMain/Program.cs
+++ GotorzProjectMain/GotorzProjectMain/Program.cs
@@ -99,12 +99,15 @@ else
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
+    app.UseMigrationsEndPoint();
 	app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+
 
 app.MapRazorComponents<App>()
 	.AddInteractiveServerRenderMode()
