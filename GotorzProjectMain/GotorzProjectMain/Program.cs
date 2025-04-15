430ec03aac13d9b988ed454c98f5226024a994e6
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2025-04-15 16:26:15 +0200

    fix: bugfix

--- GotorzProjectMain/GotorzProjectMain/Program.cs
+++ GotorzProjectMain/GotorzProjectMain/Program.cs
@@ -99,7 +99,6 @@ else
 	app.UseExceptionHandler("/Error", createScopeForErrors: true);
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
-    app.UseMigrationsEndPoint();
 	app.UseMigrationsEndPoint();
 }
 
