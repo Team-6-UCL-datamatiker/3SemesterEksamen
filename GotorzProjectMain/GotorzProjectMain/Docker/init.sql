a9cba5379f912a96c35b0e8bf3713694c573fc3a
Jesper Helstrup <jehe63306@edu.ucl.dk>
2025-04-25 16:05:10 +0200

    feat: added docker support

---
+++ GotorzProjectMain/GotorzProjectMain/Docker/init.sql
@@ -0,0 +1,15 @@
+﻿CREATE DATABASE GotorzDatabase;
+GO
+
+USE GotorzDatabase;
+GO
+
+--CREATE TABLE Users (
+--    Id INT PRIMARY KEY IDENTITY,
+--    Username NVARCHAR(100),
+--    Email NVARCHAR(100)
+--);
+GO
+
+--INSERT INTO Users (Username, Email) VALUES ('testuser', 'test@example.com');
+GO
