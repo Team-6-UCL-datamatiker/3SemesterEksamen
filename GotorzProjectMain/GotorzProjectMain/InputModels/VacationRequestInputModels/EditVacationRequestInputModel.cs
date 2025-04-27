0d4dcd483d422deb8d1019e05e0136d19c48e4e7
JonathanJarl <jarl63307@edu.ucl.dk>
2025-04-27 15:07:06 +0200

    feat. DBContextFactory deleted, inputmodel added,  trycatch added

---
+++ GotorzProjectMain/GotorzProjectMain/InputModels/VacationRequestInputModels/EditVacationRequestInputModel.cs
@@ -0,0 +1,11 @@
+﻿using GotorzProjectMain.Models;
+
+namespace GotorzProjectMain.InputModels.VacationRequestInputModels
+
+{
+    public class EditVacationRequestInputModel : VacationRequestBaseInputModel
+    {
+        public Status Status { get; set; } = Status.PendingRequest;
+
+    }
+}
