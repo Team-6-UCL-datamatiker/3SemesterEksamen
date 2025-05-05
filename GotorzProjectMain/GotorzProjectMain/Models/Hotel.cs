caf4eb563a48c59f1a2ca47d7900003c75590a4c
Jakob Baggesgaard Seeberg <jbse59225@edu.ucl.dk>
2025-05-05 09:23:48 +0200

    feat: added Hotel model class

---
+++ GotorzProjectMain/GotorzProjectMain/Models/Hotel.cs
@@ -0,0 +1,15 @@
+﻿namespace GotorzProjectMain.Models;
+
+public class Hotel
+{
+    public string? IataCode { get; set; }
+    public string? Name { get; set; }
+    public int? DuplicateId { get; set; }
+    public string? HotelId { get; set; }
+    public string? CountryCode { get; set; }
+    public GeoCode? Location { get; set; }
+    public Distance? Distance { get; set; }
+}
+
+public sealed record GeoCode(double? Latitude, double? Longitude);
+public sealed record Distance(double? Value, string? Unit);
