88b1de1e7eaa17f43eb913e17ef587807401f488
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2025-05-21 13:09:13 +0200

    feat: cleans up grouping in program.cs

--- GotorzProjectMain/GotorzProjectMain/Program.cs
+++ GotorzProjectMain/GotorzProjectMain/Program.cs
@@ -36,23 +36,35 @@ builder.Services.AddQuickGridEntityFrameworkAdapter();
 
 builder.Services.AddScoped<IRateLimiter, RateLimiter>();
 builder.Services.AddSingleton<ICityLookupService, CityLookupService>();
+
+//Used for getting the user data everytime an employee or customer is loaded
+builder.Services.AddScoped<IExtendedUserService, ExtendedUserService>();
+builder.Services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+builder.Services.AddScoped<VacationRequestSignalRService>();
+
+// Service to handle hotel API
 builder.Services.AddHttpClient<IAmadeusHotelAPIService, AmadeusHotelAPIService>(client =>
 {
 	client.BaseAddress = new Uri("https://api.amadeus.com/");
 });
 
-//Used for getting the user data everytime an employee or customer is loaded
-builder.Services.AddScoped<IExtendedUserService, ExtendedUserService>();
+// Service to handle flight API
+builder.Services.AddHttpClient<IFlightService, FlightService>(client =>
+{
+	client.BaseAddress = new Uri("https://serpapi.com/");
+});
+
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// Identity
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 
 
-builder.Services.AddScoped<VacationRequestSignalRService>();
 
 builder.Services.AddAuthentication(options =>
 	{
@@ -80,15 +92,6 @@ else
 	builder.Services.AddScoped<IEmailSender<ApplicationUser>, GmailEmailSender>();
 }
 
-// Service to handle flight API
-builder.Services.AddHttpClient<IFlightService, FlightService>(client =>
-{
-	client.BaseAddress = new Uri("https://serpapi.com/");
-});
-
-// Service to see who is logged in
-builder.Services
-	.AddScoped<ICurrentUserService, CurrentUserService>();
 
 
 // Authorization policies
