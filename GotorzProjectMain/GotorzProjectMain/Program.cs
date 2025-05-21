27ec833454dd8b2447832e2579b78436ad2c1f12
Jakob Baggesgaard Seeberg <jbse59225@edu.ucl.dk>
2025-05-21 16:50:57 +0200

    feat: Remade VacationRequestHub til serversidelogik(?)

--- GotorzProjectMain/GotorzProjectMain/Program.cs
+++ GotorzProjectMain/GotorzProjectMain/Program.cs
@@ -51,9 +51,6 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 
-
-builder.Services.AddScoped<VacationRequestSignalRService>();
-
 builder.Services.AddAuthentication(options =>
 	{
 		options.DefaultScheme = IdentityConstants.ApplicationScheme;
@@ -108,8 +105,7 @@ builder.Services.AddAuthorization(options =>
 });
 
 builder.Services.AddSignalR();
-
-
+builder.Services.AddSingleton<IVacationRequestNotifier, VacationRequestNotifier>();
 
 builder.Services.AddResponseCompression(opts =>
 {
